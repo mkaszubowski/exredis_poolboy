e1777292d51f5d3477f83248c9d3f4aba6c867ef
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2016-12-19 15:50:13 +0100

    Add package info

--- mix.exs
+++ mix.exs
@@ -4,9 +4,11 @@ defmodule ExredisPoolboy.Mixfile do
   def project do
     [app: :exredis_poolboy,
      version: "0.2.0",
+     description: "Wrapper around exredis using poolboy",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package(),
      deps: deps()]
   end
 
@@ -19,6 +21,15 @@ defmodule ExredisPoolboy.Mixfile do
     [
       {:exredis, ">= 0.2.2"},
       {:poolboy, "~> 1.5"}
+    ]
+  end
+
+  defp package do
+    [
+      mainainers: ["Maciej Kaszubowski"],
+      files: ["lib", "mix.exs", "README.md"],
+      licences: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mkaszubowski/exredis_poolboy"}
     ]
   end
 end
