93369a943558197e6817549d4169c8eda08fa39a
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2016-12-19 12:45:30 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExredisPoolboy.Mixfile do
 
   def project do
     [app: :exredis_poolboy,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
